23568a8a31fa3651bddfc7f3a97aca9943beb14d
Jcandido17 <jonathacandido17@gmail.com>
2023-11-17 14:51:37 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+function togglemode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  //pegar a tag img
+  const img = document.querySelector("#profile img")
+  //substituir a imagem
+  if (html.classList.contains("light")) {
+    //se tiver light mode, adicionar a imagem light
+    img.setAttribute("src", "./assets/jssilva-.png")
+  } else {
+    img.setAttribute("src", "./assets/jscandido.png")
+  }
+}
